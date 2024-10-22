08824bcb29da9a7c25d51d128fca8cbe20ed3457
Marcos Astudillo <antonio.ac86@hotmail.com>
2024-10-22 16:32:33 -0400

    Finished exercise-1.7, added average & positive information

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -13,11 +13,18 @@ import { useState } from 'react'
   )
 
 const Statistic = (props) => {
+  console.log(props)
+    const total = props.good + props.neutral + props.bad
+    const average = total === 0 ? 0 : (props.good - props.bad) / total
+    const positive = total === 0 ? 0 : (props.good * 100 ) / total
+
   return (
     <ul>
       <li>Good: {props.good} </li>
       <li>Neutral: {props.neutral} </li>
       <li>Bad: {props.bad}</li>
+      <li>Averange:{average}</li>
+      <li>Positive: {positive} %</li>
     </ul>
   )
 }
@@ -27,7 +34,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  
+  const values = [good, neutral, bad]
   return (
     <div>
       <Title value='Give Feedback'/>
@@ -35,7 +42,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
       <Button handleClick={() => setBad(bad + 1)} text='bad' />
       <Title value='Statistics' />
-      <Statistic good={good} neutral={neutral} bad={bad}/>
+      <Statistic good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }

c4fb4cea80970e8207a81761ac364f3cdeceac2d
Marcos Astudillo <antonio.ac86@hotmail.com>
2024-10-22 15:05:42 -0400

    Finished exercise-1.6, creating template, added buttons and showing information

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -1,14 +1,40 @@
 import { useState } from 'react'
 
+  const Title = (props) => {
+    return (
+      <h1>{props.value}</h1>
+    )
+  }
+  const Button = (props) => (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+
+const Statistic = (props) => {
+  return (
+    <ul>
+      <li>Good: {props.good} </li>
+      <li>Neutral: {props.neutral} </li>
+      <li>Bad: {props.bad}</li>
+    </ul>
+  )
+}
+
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  
   return (
     <div>
-      code here
+      <Title value='Give Feedback'/>
+      <Button handleClick={() => setGood(good + 1)} text='good'/>
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      <Title value='Statistics' />
+      <Statistic good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }

ff63e1c51008721e6dfa47aad5077354cf8a9d2c
Marcos Astudillo <antonio.ac86@hotmail.com>
2024-11-11 17:00:27 -0500

    Finished exercise-2.13, update code, and splitted each module

--- Part2/phonebook/src/Services/personList.js
+++ Part2/phonebook/src/Services/personList.js
@@ -1,16 +1,20 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3001/persons'
 
+// Show all the persons
 const getAll = () => {
     const request = axios.get(baseUrl)
     return request.then(response => response.data)
 }
 
+// Create a new person object
 const create = newObject => {
     const request = axios.post(baseUrl, newObject)
     return request.then(response => response.data)
 }
 
+
+// Update a new person object (non-implemented on APP yet)
 const update = (id, newObject) => {
     const request = axios.put(`${baseUrl}/${id}`, newObject)
     return request.then(response => response.data)

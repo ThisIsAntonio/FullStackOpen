b2afc48f9ee9f074b946ef63d564e99efcbbd4e8
Marcos Astudillo <antonio.ac86@hotmail.com>
2024-11-18 13:24:24 -0500

    Finished exercise-3.4, added delete function, tested on postman and added on requests folder

--- Part3/phonebook/index.js
+++ Part3/phonebook/index.js
@@ -21,6 +21,11 @@ persons = [
         "id": 4,
         "name": "Mary Poppendieck", 
         "number": "39-23-6423122"
+    },
+    { 
+        "id": 5,
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122"
     }
 ]
 
@@ -55,6 +60,13 @@ app.get('/api/persons/:id', (request, response) => {
     } else {
         response.status(404).end()
     }
+})
+
+// Remove one person
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
 })
 
 const PORT = 3001

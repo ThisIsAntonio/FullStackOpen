2636e9685a2f09c1755e1a524d0225c1aad61f38
Marcos Astudillo <antonio.ac86@hotmail.com>
2024-11-18 13:19:11 -0500

    Finished exercise-3.3, added show info of 1 person, and as well show error if this doesn't exist

--- Part3/phonebook/index.js
+++ Part3/phonebook/index.js
@@ -39,9 +39,22 @@ app.get('/info', (request, response) => {
 })
 
 
+// REST API Persons
+
 // Showing list of people
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+// Showing one person
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
 const PORT = 3001
